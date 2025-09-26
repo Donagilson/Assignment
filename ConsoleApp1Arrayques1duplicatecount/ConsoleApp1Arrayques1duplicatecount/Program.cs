18ef182bdb93cb794cffe00ae8ed7f8112bf99fd
Donagilson <donagilson2002@gmail.com>
2025-09-26 09:44:10 +0530

    Add files via upload

---
+++ ConsoleApp1Arrayques1duplicatecount/ConsoleApp1Arrayques1duplicatecount/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SimpleDuplicateCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the size of the array: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            Console.WriteLine("Enter the elements of the array:");
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int duplicateCount = 0;
+
+            // Compare each element with the rest of the elements
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        duplicateCount++;
+                        break; // stop after finding first duplicate of arr[i]
+                    }
+                }
+            }
+
+            Console.WriteLine("Total number of duplicate elements: " + duplicateCount);
+        }
+    }
+}
+
---
+++ ConsoleApp1anothermethodofnumberdivisibleby9between100and200/ConsoleApp1anothermethodofnumberdivisibleby9between100and200/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleAppDivisibleBy9
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Numbers between 100 and 200 divisible by 9:");
+
+            for (int i = 100; i <= 200; i++)
+            {
+                if (i % 9 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+            Console.WriteLine(); // new line at end
+        }
+    }
+}
