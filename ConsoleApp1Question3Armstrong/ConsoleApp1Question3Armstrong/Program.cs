12bcdcb5601dd29ec5e48709f824d8e12a918217
Donagilson <donagilson2002@gmail.com>
2025-09-27 09:06:26 +0530

    Add files via upload

---
+++ ConsoleApp1Question3Armstrong/ConsoleApp1Question3Armstrong/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleAppArmstrongNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (utility.numbercheck.IsArmstrong(num))
+                Console.WriteLine($"{num} is an Armstrong number.");
+            else
+                Console.WriteLine($"{num} is NOT an Armstrong number.");
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1Question3Armstrong/ConsoleApp1Question3Armstrong/Utility/numbercheck.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utility
+{
+    public static class numbercheck
+    {
+        public static bool IsArmstrong(int num)
+        {
+            if (num < 0)
+                return false; // Armstrong numbers are non-negative
+
+            int sum = 0;
+            int temp = num;
+
+            // Count digits
+            int digits = num.ToString().Length;
+
+            // Calculate sum of digits raised to the power of digits
+            while (temp > 0)
+            {
+                int digit = temp % 10;
+                sum += (int)Math.Pow(digit, digits);
+                temp /= 10;
+            }
+
+            return sum == num;
+        }
+    }
+}
+
